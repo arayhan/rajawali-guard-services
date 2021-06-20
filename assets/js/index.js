252f179cbc9abee0ec2304bb9b7603155d737d7b
Rayhan <rayhan@badr.co.id>
2021-06-21 03:52:07 +0700

    update About section

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,4 +6,18 @@ $(document).ready(function () {
 	$("#headerBackdrop").click(function () {
 		$("#headerBackdrop, #headerSidenav").removeClass("active");
 	});
+
+	$("#aboutVisiButton").click(function () {
+		$("#aboutVisiButton").removeClass("outline");
+		$("#aboutMisiButton").addClass("outline");
+		$("#aboutVisi").removeClass("hidden");
+		$("#aboutMisi").addClass("hidden");
+	});
+
+	$("#aboutMisiButton").click(function () {
+		$("#aboutVisiButton").addClass("outline");
+		$("#aboutMisiButton").removeClass("outline");
+		$("#aboutVisi").addClass("hidden");
+		$("#aboutMisi").removeClass("hidden");
+	});
 });
